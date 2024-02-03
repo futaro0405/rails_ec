f9fa88e61f516d3a85e86eb048786936f1e1542c
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-02-03 13:01:00 +0900

    created views for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,30 @@
 class ItemsController < ApplicationController
+  add_flash_types :success, :info, :warning, :danger
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @items = Item.all
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    item.save!
+    redirect_to item_url, notice: "¤•iu#{item.name}v‚ð“o˜^‚µ‚Ü‚µ‚½B"
+  end
+
+  def edit; end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:id, :name, :price, :description, :image)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  def thumnail
+    image.variant(resize_to_limit: [300, 300])
+  end
 end
