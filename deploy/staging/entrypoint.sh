2ad1e209fbcde8ded1f03fa14a262ff7b5bc9e1c
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-02-03 21:42:14 +0900

    fixed entrypoint

--- deploy/staging/entrypoint.sh
+++ deploy/staging/entrypoint.sh
@@ -6,7 +6,7 @@ rm -f /myapp/tmp/pids/server.pid
 
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
 bundle exec rails db:seed_fu
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
