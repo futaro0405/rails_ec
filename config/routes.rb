cc25a46f145f0a6845878300b0a30969f5a552d3
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-02-05 21:05:38 +0900

    created users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  namespace :admin do
+    resources :users
+  end
+  
   resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
