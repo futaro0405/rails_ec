64dc8f93ca819074515b276e9733c6cc67577064
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-02-23 14:42:41 +0900

    fixed dbseed

--- db/schema.rb
+++ db/schema.rb
@@ -67,16 +67,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_14_132755) do
     t.index ["name"], name: "index_items_on_name", unique: true
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.string "password_digest", null: false
-    t.boolean "admin", default: false, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name", "email"], name: "index_users_on_name_and_email", unique: true
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cart_items", "carts"
