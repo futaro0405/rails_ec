b96c35d5b33c27386ef23cbf03dc8a4391bdbd69
github futaro0405 <fkswtkm0405.web@gmail.com>
2024-03-08 16:59:30 +0900

    fixed active mailer for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,15 +71,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = {  host: 'localhost', port: 8000 }
+  config.action_mailer.default_url_options = { protocol: 'https', host:'https://ecsite-web.onrender.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
     port: 587,
-    user_name: Rails.application.credentials.dig(:gmail, :email),
-    password: Rails.application.credentials.dig(:gmail, :app_password),
-    authentication: :login
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
